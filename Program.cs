3e853229f2327872f71e3ecb3f885ebfc29cf6f1
kinko6 <85136008+kinko6@users.noreply.github.com>
2024-09-13 08:49:23 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("digite o numero: ");
+int num1 = Convert.ToInt16(Console.ReadLine());
+int num2 = 0;
+
+Console.WriteLine("digite a operacao (+, -, *, /, =):");
+string operacao = Console.ReadLine();
+
+if (operacao != "=") { 
+Console.Write("digite o segundo numero: ");
+num2 = Convert.ToInt16(Console.ReadLine());
+}
+
+switch (operacao)
+{
+    case "+":
+        return num1 + num2;
+    case "-":
+        return num1 - num2;
+    case "*":
+        return num1 * num2;
+    case "/":
+        if (num2 != 0)
+        {
+            return num1 / num2;
+        }
+        else
+        {
+            throw new DivideByZeroException("impossivel dividir por 0");
+        }
+    case "=":
+        return num1;
+    default:
+        throw new ArgumentException("operacao invalidada");
+}
